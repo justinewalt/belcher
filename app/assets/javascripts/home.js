6fdccaf90868d2323496c0c42bfe418116827687
Brandon Huynh <brandonvhuynh@gmail.com>
2016-04-05 21:28:55 -0600

    navigation bar, responsive, flash messages, font, general styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,9 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  return $('.flash').show(function() {
+    var flash;
+    flash = $(this);
+    return setTimeout(function() {
+      return flash.slideToggle();
+    }, 3000);
+  });
+});
