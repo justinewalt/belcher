27beab5ed59d1e9dc378d4ae57bcf2c2362b25ea
Garrett Stot <garrettstott@gmail.com>
2016-04-07 16:06:12 -0600

    changed contact

--- app/assets/javascripts/components/Contact.js.jsx
+++ app/assets/javascripts/components/Contact.js.jsx
@@ -1,6 +1,17 @@
 class Contact extends React.Component {
   constructor(props) {
     super(props);
+    this.sendMessage = this.sendMessage.bind(this);
+  }
+
+  sendMessage(e) {
+    e.preventDefault();
+    email = this.refs.email.value;
+    subject = this.refs.subject.value;
+    message = this.refs.message.value;
+    console.log(email);
+    console.log(subject);
+    console.log(message);
   }
 
   render() {
@@ -8,16 +19,17 @@ class Contact extends React.Component {
       <div className="twelve columns text-center">
       <hr />
         <h3 className="text-center">Contact</h3>
-        <p>This is the Contact info paragraph</p>
-         <form>
-          <input type="email" placeholder="Your Email Address" required={true} />
+        <p className="text-center">This is the Contact info paragraph</p>
+        <div className="offset-by-three six columns">
+         <form className="contact-form" onSubmit={this.sendMessage} >
+          <input ref="email" className="subject-input" type="email" placeholder="Your Email (required)" required={true} />
+          <input ref="subject" className="subject-input" type="text" placeholder="Subject (required)" required={true} />
           <br />
-          <input type="text" placeholder="Subject" required={true} />
-          <br />
-          <input type="text" placeholder="Message" required={true} />
+          <textarea ref="message" className="message-input" placeholder="Message (required)" required={true}></textarea>
           <br />
           <input type="submit" name="Send" className="button-primary" />
-        </form>
+         </form>
+        </div>
       </div>
     );
   }
