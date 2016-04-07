359fadc96485a6dee21e1be1be06c8f4a996b04c
Garrett Stot <garrettstott@gmail.com>
2016-04-06 20:19:09 -0600

    added contact and about components

---
+++ app/assets/javascripts/components/Index.js.jsx
@@ -0,0 +1,14 @@
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+     return(
+      <div>
+        <Contact />
+        <About />
+      </div>
+    );
+  }
+}
