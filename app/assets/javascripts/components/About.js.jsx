81183a5f627c45f8fdcf0f3dab5757dcefdc0728
Garrett Stot <garrettstott@gmail.com>
2016-04-06 20:19:09 -0600

    added contact and about components

---
+++ app/assets/javascripts/components/About.js.jsx
@@ -0,0 +1,15 @@
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="twelve columns">
+        <h3 className="text-center">Contact</h3>
+        <hr />
+        <p>This is the contact info paragraph</p>
+      </div>
+    );
+  }
+}
---
+++ app/assets/javascripts/components/Contact.js.jsx
@@ -0,0 +1,15 @@
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="twelve columns">
+        <h3 className="text-center">About</h3>
+        <hr />
+        <p>This is the about info paragraph</p>
+      </div>
+    );
+  }
+}
