58a75edcff28572585631ca17a017bec1a78531a
Brandon Huynh <brandonvhuynh@gmail.com>
2016-04-07 16:25:48 -0600

    navmenu and some component refactoring T#

--- app/assets/javascripts/components/hamburger.js.jsx
+++ app/assets/javascripts/components/hamburger.js.jsx
@@ -2,32 +2,12 @@ class Hamburger extends React.Component {
   constructor(props){
     super(props);
     this.state = {isOpen: false};
-    this.toggleOpen = this.toggleOpen.bind(this);
-  }
-
-  toggleOpen(){
-    this.setState({isOpen: !this.state.isOpen})
-  }
-
-  menu(){
-    if (this.state.isOpen) {
-      return(<div className="hamburger-menu">
-                <div className="hamburger-content">
-                  <a>Profile</a>
-                  <a>Preferences</a>
-                  <a>About</a>
-                  <a>Contact Us</a>
-                </div>
-             </div>
-            );
-    }
   }
 
   render(){
     return(
       <div>
-        <p onClick={this.toggleOpen} className="hamburger"><a><i className="fa fa-bars fa-3x ham-icon"></i></a></p>
-        {this.menu()}
+        <p onClick={this.props.toggleMenuOpen} className="hamburger"><i className="fa fa-bars fa-3x ham-icon"></i></p>
       </div>
     );
   }
