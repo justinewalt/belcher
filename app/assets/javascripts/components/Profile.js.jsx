1eaf941e2b1fc9ff9aee755b36e92de38a53f34f
Garrett Stot <garrettstott@gmail.com>
2016-04-07 13:40:41 -0600

    couple more changes to components

---
+++ app/assets/javascripts/components/Profile.js.jsx
@@ -0,0 +1,14 @@
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="twelve columns">
+        <hr />
+        <h1 className="text-center">Profile</h1>
+      </div>
+    );
+  }
+}
