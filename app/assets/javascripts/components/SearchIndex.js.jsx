ccb83b69ac27b49d8f315e8c9823b9348cc1a983
Garrett Stot <garrettstott@gmail.com>
2016-04-07 20:05:27 -0600

    changed search component

--- app/assets/javascripts/components/SearchIndex.js.jsx
+++ app/assets/javascripts/components/SearchIndex.js.jsx
@@ -10,11 +10,15 @@ class SearchIndex extends React.Component {
           <div className="find-food-button offset-by-four four columns">
             <p>Find Me Food</p>
           </div>
-          <div className="search-index-buttons twelve columns">
+          <div className="search-index-buttons price-button twelve columns">
+            <h5>Price</h5>
             <button>$</button>
             <button>$$</button>
             <button>$$$</button>
             <button>$$$$</button>
+          </div>
+          <div className="search-index-buttons distance-button twelve columns">
+            <h5>Distance</h5>
             <button>Walking</button>
             <button>5 Miles</button>
             <button>10 Miles</button>
