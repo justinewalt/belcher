b242ab99e1a58ca7eb68c5f0f866f58ce93899f5
Garrett Stot <garrettstott@gmail.com>
2016-04-07 20:23:34 -0600

    added hr to about

--- app/assets/javascripts/components/About.js.jsx
+++ app/assets/javascripts/components/About.js.jsx
@@ -6,6 +6,7 @@ class About extends React.Component {
   render() {
     return (
       <div className="twelve columns">
+      <hr />
         <h3 className="text-center">About</h3>
         <p className="text-center">This is the About info paragraph</p>
         <div className="text-center">
