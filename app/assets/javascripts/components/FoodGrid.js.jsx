cbe00c403e7eeb3f4fc9028f17c35b9c47f795dd
GavMan1995 <gavyncaldwell@gmail.com>
2016-04-11 17:19:32 -0600

    working search function, with buttons and search bar, chooses your place to eat

--- app/assets/javascripts/components/FoodGrid.js.jsx
+++ app/assets/javascripts/components/FoodGrid.js.jsx
@@ -5,26 +5,26 @@ class FoodGrid extends React.Component {
 
   render () {
     return (
-      <div className="twelve columns"> 
+      <div className="twelve columns">
         <hr />
         <div>
           <h3 className="text-center">Preferences</h3>
         </div>
         <div className="food-button-input offset-by-three six columns">
           <div className="twelve columns">
-            <button>American</button>
-            <button>Chinese</button>
-            <button>French</button>
+            <button onClick={() => this.props.searchParams("american")}>American</button>
+            <button onClick={() => this.props.searchParams("chinese")}>Chinese</button>
+            <button onClick={() => this.props.searchParams("french")}>French</button>
           </div>
           <div className="twelve columns">
-            <button>German</button>
-            <button>Italian</button>
-            <button>Japanese</button>
+            <button onClick={() => this.props.searchParams("german")}>German</button>
+            <button onClick={() => this.props.searchParams("italian")}>Italian</button>
+            <button onClick={() => this.props.searchParams("japanese")}>Japanese</button>
           </div>
           <div className="twelve columns">
-            <button>Mexican</button>
-            <button>Thai</button>
-            <button>Vietnamese</button>
+            <button onClick={() => this.props.searchParams("mexican")}>Mexican</button>
+            <button onClick={() => this.props.searchParams("thai")}>Thai</button>
+            <button onClick={() => this.props.searchParams("vietnamese")}>Vietnamese</button>
           </div>
         </div>
       </div>
