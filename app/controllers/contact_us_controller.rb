7dfa7759ea19833d1c86f1791c3caf0023b341a5
Garrett Stot <garrettstott@gmail.com>
2016-04-11 17:10:07 -0600

    added flash message to contact

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -1,6 +1,7 @@
 class ContactUsController < ApplicationController
   def new_contact
     ContactUsMailer.new_contact(params[:body]).deliver_now
+    flash[:notice] = "Message Sent!"
     redirect_to root_path
   end
 end
