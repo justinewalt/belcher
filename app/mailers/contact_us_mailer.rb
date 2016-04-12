7d6294d72f706b4a8d4e9c41e810b766d24ad842
Garrett Stot <garrettstott@gmail.com>
2016-04-12 14:58:09 -0600

    changed some mailer methods

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -4,6 +4,4 @@ class ContactUsMailer < ApplicationMailer
  def new_contact(body)
    mail(to: ENV['MAIL_FROM'], subject: 'Message from Belcher', body: body)
  end
-
- handle_asynchronously :new_contact
 end
