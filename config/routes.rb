5cf4b9f203f6c7acc4d86379db578bf5e6c263c3
GavMan1995 <gavyncaldwell@gmail.com>
2016-04-12 15:56:10 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
-
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   post 'contact_us', to: 'contact_us#new_contact'
 
-#==== GET ROUTES
   get '/search', to: 'home#search'
-
-  
 end
