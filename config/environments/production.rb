182052974b751bd221b6f64d5f3fe554e7d885de
Garrett Stot <garrettstott@gmail.com>
2016-04-13 14:34:37 -0600

    gmail fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:            'smtp.gmail.com',
     port:                587,
+    domain:              'gmail.com',
     user_name:           ENV['GMAIL_USERNAME'],
     password:            ENV['GMAIL_PASSWORD'],
-    authentication:      'plain',
+    authentication:      :login,
     enable_starttls_auto: true
   }
   # Settings specified here will take precedence over those in config/application.rb.
