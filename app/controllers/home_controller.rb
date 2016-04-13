fe9b2ad026f70e415105580d049d79eba72135e9
GavMan1995 <gavyncaldwell@gmail.com>
2016-04-13 10:59:24 -0600

    fixed the loop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,13 +8,13 @@ class HomeController < ApplicationController
     distance = params["distance"].to_i
     lat = params["lat"].to_f
     long = params["lng"].to_f
-    location = Geocoder.search("#{lat}, #{long}")
-    origin = location.first.formatted_address
-    search_params = params["search"]
-    search_params = search.join(" ")
-    spots = @client.spots(lat, long, :radius =>  distance, :name => search_params, :types => ['restaurant', 'food', 'meal_takeaway', 'meal_delivery', 'cafe', 'bakery'], :exclude => ['grocery_or_supermarket'] )
+    origin = Geocoder.search("#{lat}, #{long}").first.formatted_address
+    search_params = params["search"].join(" ")e
+    spots = @client.spots(lat, long, :radius =>  distance, :name => search_params,
+                          :types => ['restaurant', 'food', 'meal_takeaway', 'meal_delivery', 'cafe', 'bakery'],
+                          :exclude => ['grocery_or_supermarket'] )
     filtered_spots = spots.reject { |spot| spot.price_level.to_i > price}
-    render json: {filtered_spots: filtered_spots, origin: origin }
+    render json: {filtered_spots: filtered_spots, origin: origin}
   end
 
   private
