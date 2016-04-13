09343309d9dc22d19cd4fbedbef95ae5c012acc1
Brandon Huynh <brandonvhuynh@gmail.com>
2016-04-13 14:08:15 -0600

    fixed merge conflict

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,6 @@ class HomeController < ApplicationController
     spots = @client.spots(lat, long, :radius =>  distance, :name => search_params,
                           :types => ['restaurant', 'food', 'meal_takeaway', 'meal_delivery', 'cafe', 'bakery'],
                           :exclude => ['grocery_or_supermarket'] )
-    binding.pry                      
     spots = spots.reject { |spot| spot.price_level.to_i > price}
     filtered_spots = spots.reject { |spot| spot.opening_hours == false}
     render json: {filtered_spots: filtered_spots, origin: origin}
