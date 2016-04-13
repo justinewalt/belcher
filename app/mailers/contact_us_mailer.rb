598fe93ae2ad089c15beb07be766bf5512c54a5d
Garrett Stot <garrettstott@gmail.com>
2016-04-13 15:00:04 -0600

    fixed mailer

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -2,6 +2,7 @@ class ContactUsMailer < ApplicationMailer
  default from: ENV['MAIL_FROM']
 
  def new_contact(body)
-   mail(to: ENV['MAIL_FROM'], subject: 'Message from Belcher', body: body)
+   @body = body
+   mail(to: ENV['MAIL_FROM'], subject: 'Message from Belcher')
  end
 end
