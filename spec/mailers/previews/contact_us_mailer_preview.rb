27b1f91224a91b45b32178069270498f356f8d91
Garrett Stot <garrettstott@gmail.com>
2016-04-13 15:00:04 -0600

    fixed mailer

--- spec/mailers/previews/contact_us_mailer_preview.rb
+++ spec/mailers/previews/contact_us_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_us_mailer
 class ContactUsMailerPreview < ActionMailer::Preview
-
+ def new_contact
+   body = 'body'
+   ContactUsMailer.new_contact(body)
+ end
 end
--- test/mailers/previews/contact_us_mailer_preview.rb
+++
@@ -1,5 +0,0 @@
-class ContactUsMailerPreview < ActionMailer::Preview
- def new_contact_preview
-   ContactUsMailer.new_contact.deliver_now
- end
-end
