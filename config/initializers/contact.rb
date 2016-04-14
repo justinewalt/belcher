55fb80f57f3b29eb8ed1d0c609666b1b72c1fddc
Garrett Stot <garrettstott@gmail.com>
2016-04-14 14:30:04 -0600

    changed contact initializer

--- config/initializers/contact.rb
+++ config/initializers/contact.rb
@@ -1,8 +1,10 @@
-begin
- mail = "#{Rails.root}/config/contact.yml"
- YAML.load_file(mail).each do |key, value|
-   ENV[key] = value
- end
-rescue
- raise "No contact.yml config file in your config directory."
+if Rails.env.development?
+  begin
+   mail = "#{Rails.root}/config/contact.yml"
+   YAML.load_file(mail).each do |key, value|
+     ENV[key] = value
+   end
+  rescue
+   raise "No contact.yml config file in your config directory."
+  end
 end
