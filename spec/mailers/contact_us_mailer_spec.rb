7a5f90f9744e07869ea606706e5660d86db7df9a
Garrett Stot <garrettstott@gmail.com>
2016-04-14 17:12:02 -0600

    mailer test

--- spec/mailers/contact_us_mailer_spec.rb
+++ spec/mailers/contact_us_mailer_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe ContactUsMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    body = "body"
+    ContactUsMailer.new_contact(body)
+  end
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
+   it 'should send an email' do
+    expect(ActionMailer::Base.deliveries.count).to eq(0)
+  end
 end
