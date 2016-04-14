88e873774bc627b6dba73d0e944bf2f47c410665
Garrett Stot <garrettstott@gmail.com>
2016-04-14 16:10:56 -0600

    removed example code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '.from_omniauth' do 
     
-  end
 end
-
-
-  # it 'returns only active users' do
-  #   # setup
-  #   active_user = create(:user, active: true)
-  #   non_active_user = create(:user, active: false)
-
-  #   # exercise
-  #   result = User.active
-
-  #   # verify
-  #   expect(result).to eq [active_user]
-
-  #   # teardown is handled for you by RSpec
