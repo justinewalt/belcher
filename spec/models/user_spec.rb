06e0a55e38ed3fed4bbbbf3bf6d87139df59a9d6
Garrett Stot <garrettstott@gmail.com>
2016-04-14 16:01:08 -0600

    commented out some example code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,16 +7,15 @@ RSpec.describe User, type: :model do
 end
 
 
-  it 'returns only active users' do
-    # setup
-    active_user = create(:user, active: true)
-    non_active_user = create(:user, active: false)
+  # it 'returns only active users' do
+  #   # setup
+  #   active_user = create(:user, active: true)
+  #   non_active_user = create(:user, active: false)
 
-    # exercise
-    result = User.active
+  #   # exercise
+  #   result = User.active
 
-    # verify
-    expect(result).to eq [active_user]
+  #   # verify
+  #   expect(result).to eq [active_user]
 
-    # teardown is handled for you by RSpec
-  end
+  #   # teardown is handled for you by RSpec
