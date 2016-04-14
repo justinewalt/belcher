987a3e6f8d887f08a57a3b91a124a5a52dad9e54
Garrett Stot <garrettstott@gmail.com>
2016-04-14 15:37:39 -0600

    fixed helper

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.from_omniauth' do 
+    
+  end
 end
+
+
+  it 'returns only active users' do
+    # setup
+    active_user = create(:user, active: true)
+    non_active_user = create(:user, active: false)
+
+    # exercise
+    result = User.active
+
+    # verify
+    expect(result).to eq [active_user]
+
+    # teardown is handled for you by RSpec
+  end
