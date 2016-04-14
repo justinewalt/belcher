48b2201cdf6e45d542b59a5cd0ef436321d7e71f
GavMan1995 <gavyncaldwell@gmail.com>
2016-04-14 17:34:56 -0600

    search, yes and no working, responsive size for maps

---
+++ app/assets/javascripts/components/ResultsPage.js.jsx
@@ -0,0 +1,37 @@
+class ResultsPage extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    if(this.props.results.length != 0){
+      let url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${this.props.result.photos[0].photo_reference}&key=AIzaSyBblRBZp_9JKVUeK-HKRcW4_EY160-CmeU`
+      let price = this.props.result.price_level
+      if (price == 3) {
+        price = 'Price: $$$'
+      }else if (price == 2) {
+        price = 'Price: $$'
+      }else if (price == 1) {
+        price = 'Price: $'
+      };
+
+
+      return(
+        <div className="text-center">
+          <h1>{this.props.result.name}</h1>
+
+          <img src={url} />
+          <br />
+          <br />
+          <button onClick={this.props.newResult}>No</button>
+          <button onClick={this.props.clickYes}>Yes</button>
+        </div>
+      );
+    }else {
+      return(
+        <div></div>
+      );
+    }
+  }
+
+}
