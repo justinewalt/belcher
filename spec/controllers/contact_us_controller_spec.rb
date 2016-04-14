211f7f945724066b85c1363f213d2b244dcdb318
Garrett Stot <garrettstott@gmail.com>
2016-04-14 17:12:02 -0600

    mailer test

--- spec/controllers/contact_us_controller_spec.rb
+++ spec/controllers/contact_us_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ContactUsController, type: :controller do
+
+  describe 'action calls ContactUsMailer' do 
+    it 'sends an email' do 
+  
+    end
+  end
 end
