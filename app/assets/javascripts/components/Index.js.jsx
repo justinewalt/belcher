d04a0ef7ac841287d57398725291b7f4497b0c0c
Brandon Huynh <brandonvhuynh@gmail.com>
2016-04-15 13:57:09 -0600

    nav fix

--- app/assets/javascripts/components/Index.js.jsx
+++ app/assets/javascripts/components/Index.js.jsx
@@ -1,30 +1,16 @@
 class Index extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {menuOpen: false};
-    this.toggleMenuOpen = this.toggleMenuOpen.bind(this)
-  }
-
-  toggleMenuOpen(){
-    this.setState({menuOpen: !this.state.menuOpen})
   }
 
 
   render() {
      return(
       <div>
-        <Navigation links={this.props.links}
-                    userLogIn={this.props.userLogIn}
-                    toggleMenuOpen={this.toggleMenuOpen}
-                    />
         <SearchIndex />
         <About />
         <Contact />
         <Profile />
-        <NavMenu isOpen={this.state.menuOpen}
-                  links={this.props.links} 
-                  userLogIn={this.props.userLogIn}
-                  />
       </div>
     );
   }
