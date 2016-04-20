b7e6c5ae140f0437495a4de7010ea58494055d7b
justinewalt <justinewalt@gmail.com>
2016-04-20 15:37:55 -0600

    fixing toggling on price and distance buttons

--- app/assets/javascripts/components/DistanceButton.js.jsx
+++ app/assets/javascripts/components/DistanceButton.js.jsx
@@ -1,26 +1,22 @@
 class DistanceButton extends React.Component {
   constructor(props) {
     super(props);
-    this.toggleButton = this.toggleButton.bind(this);
-    this.state = { checked: false, style: {} };
+    // this.toggleButton = this.toggleButton.bind(this);
+    this.state = { style: {} };
   }
 
-  toggleButton(){
-    let select = !this.state.checked; 
-    let style = {};
-    if (select) {
-      style = { 
-        backgroundColor: '#33C3F0',
-        color: 'white'
-      }
-    }
-    this.props.handleClick(this.props.name)
-    this.setState({ checked: select, style: style });
-  }
+  // toggleButton(){
+  //   let style = {
+  //     backgroundColor: '#33C3F0',
+  //     color: 'white'
+  //   }
+  //   this.props.handleClick(this.props.name)
+  //   this.setState({ style: style });  
+  // }
 
   render() {
     return (
-      <button onClick={this.toggleButton} style={this.state.style}>{this.props.id}</button>
+      <button onClick={this.props.toggleButton} style={this.state.style}>{this.props.id}</button>
     )
   }
 }
