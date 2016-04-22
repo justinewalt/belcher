4a75e9ad35b601faf5dcd154734440a76e1f5412
GavMan1995 <gavyncaldwell@gmail.com>
2016-04-22 13:56:48 -0600

    fixed merge conflicts :

--- app/assets/javascripts/components/ResultsPage.js.jsx
+++ app/assets/javascripts/components/ResultsPage.js.jsx
@@ -24,20 +24,20 @@ class ResultsPage extends React.Component {
 
       return(
         <div className="text-center">
-          <h1 className="result-name">{this.props.result.name}</h1>
-          <p className="map-div-text">{this.props.result.vicinity}</p>
+          <h1>{this.props.result.name}</h1>
           <img className="result_image" src={url} />
-          <p className="price-rating">{price}<span className="price-rating-span"> </span>Rating: {this.props.result.rating} </p>
+          <p className="map-div-text">{this.props.result.vicinity}</p>
+          <p className="map-div-text">{price} - Rating: {this.props.result.rating} </p>
           <br />
-          <p className="eat-here">Eat here?</p>
-          <p className="fa fa-times-circle result-button" onClick={this.props.newResult}></p>
-          <p className="fa fa-check-circle result-button" onClick={this.props.clickYes}></p>
+          <p className="map-div-text">Would you like to eat here?</p>
+          <p className="fa fa-times-circle no-class" onClick={this.props.newResult}></p>
+          <p className="fa fa-check-circle" onClick={this.props.clickYes}></p>
         </div>
       );
     }else {
       return(
         <div></div>
-      )
+      );
     }
   }
 
