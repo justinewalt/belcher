35ab6571277906a1a601c5fb99bc910e2199aa0f
justinewalt <justinewalt@gmail.com>
2016-04-25 12:01:39 -0600

    wrapped Get Directions in the div to also be clickable

--- app/assets/javascripts/components/Yes.js.jsx
+++ app/assets/javascripts/components/Yes.js.jsx
@@ -18,18 +18,19 @@ class Yes extends React.Component {
       };
       let url = `https://maps.googleapis.com/maps/api/staticmap?center=${this.props.result.lat},${this.props.result.lng}&zoom=17&size=250x250&markers=color:red|${this.props.result.lat},${this.props.result.lng}&maptype=roadmap&key=AIzaSyDjH86rcfIoaXzluTnahrfvvnLxU0920vU`
       let href = `http://maps.google.com/?q=${this.props.result.name}+${this.props.result.vicinity}`
+      
       return(
         <div className="text-center">
           <h1 className="result-name">{this.props.result.name}</h1>
           <p className="price-rating">{price}<span className="price-rating-span"> </span>Rating: {this.props.result.rating} </p>
           <p className="map-div-text">{this.props.result.vicinity}</p>
           <img src={url} className="map_frame"></img>
-          <div>
+          <a href={href} target="_blank">
             <div>
-              <a className="fa fa-chevron-circle-right" href={href} target="_blank"></a>
+              <i className="fa fa-chevron-circle-right"></i>
               <p className="direction-text">Get Directions</p>
             </div>
-          </div>
+          </a>
         </div>
       );
     }else {
